8f9b8ce5d352d7ab1865811c761fa07ee37c51c5
Klaus Roßmann <klaus@AT-NB-534.AT.LAN>
2025-11-10 11:33:48 +0100

    Initial commit

---
+++ myapi.py
@@ -0,0 +1,60 @@
+from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+students = {
+    1: {
+        "name": "john",
+        "age": 17,
+        "class": "year 12",
+    }
+}
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
+
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
+    
+@app.get("/")
+def index():
+    return {"name": "First Data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(description="The id of the student", gt=0, lt=3)):
+    return students[student_id]
+
+@app.get("/get-student-by-name/{student_id}")
+def get_student(*, student_id: int, name: Optional[str] = None, test: int):
+    for id, student in students.items():
+        if student["name"] == name:
+            return student
+    return {"Data": "Not found"}
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"Error": "Student exists"}
+    
+    students[student_id] = student
+    return students[student_id]
+
+@app.put("/upate-student/{student_id}")
+def update_student(student_id: int, student: UpdateStudent):
+    if student_id not in students:
+        return {"Error": "Student does not exists"}
+    
+    if student.name:
+        students[student_id].name = student.name
+    if student.age:
+        students[student_id].age = student.age
+    if student.year:
+        students[student_id].year = student.year
+
+    return students[student_id]
